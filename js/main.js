cb6a5d34cb44da21eb004be0e8b17fea16d90689
Aleksandr <s_a_s_h_a_5@mail.ru>
2020-05-21 03:33:35 +0300

    add page scroll

--- js/main.js
+++ js/main.js
@@ -100,4 +100,10 @@ $(document).ready(function () {
     }
     navBlock.removeClass(navBlockOpen);
   })
+
+  	//slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a, a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+		highlightSelector:"nav a"
+	});
+
 });
